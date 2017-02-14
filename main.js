54b418e103ab1dafa998e5daeb62d16224a04696
Amanda Wolgamott <akirkpatrick100@gmail.com>
2017-02-14 13:52:02 -0600

    add dom manipulations

--- main.js
+++ main.js
@@ -1,28 +1,72 @@
 
 document.querySelector("#thanks button").addEventListener('click',function(){
-  // TASK #1
+  // TASK #1  
+	alert("yowch! don\'t click me so hard!")
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
-  // TASK #2
+	  // TASK #2
+	var runningTotal = 0
+	var double = parseInt(document.querySelector("#compoundInvestment").innerHTML)
+	runningTotal = runningTotal + (double * 2)
+	console.log(runningTotal)
+	document.querySelector("#compoundInvestment").innerHTML = runningTotal
 })
+
+
 
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
+	var colorChange = document.querySelector("#circle-bw") 
+	if(colorChange.style.background === "black"){
+		colorChange.style.background = "white"
+	}
+	else {
+		colorChange.style.background = "black"
+	}
+
 })
 
-document.querySelector("#blow-up button").addEventListener('click',function(){
+
+
+
+document.querySelector("#blow-up button").addEventListener('click', function(){
   // TASK #4}
+  var sizeChange = document.querySelector(".circle-red")
+  	var styleObj = window.getComputedStyle(sizeChange)
+	console.log(styleObj.width)
+	var doubleWidthInt = parseInt(styleObj.width) * 2
+	if(parseInt(sizeChange.style.width) <= 320){
+		sizeChange.style.width = doubleWidthInt + "px"
+		var doubleHeightInt = parseInt(styleObj.height) * 2
+		sizeChange.style.height = doubleHeightInt + "px"
+	}
+	else{
+		sizeChange.style.width = '40px'
+		sizeChange.style.height = '40px'
+	}
+	
 })
 
 document.querySelector("#remove button").addEventListener('click',function(){
   // TASK #5
+  var removeInactive = document.querySelector("#userList")
+  var liArray = removeInactive.children
+  for (var index = 0; index < liArray.length; index = index + 1) {
+      if(liArray[index].classList.contains('inactive')) {
+        removeInactive.removeChild(liArray[index]) 
+        index = index - 1
+      }
+  }
 })
 
 
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
   // TASK #6
+  var flipSquares = document.querySelector("#reverse-squares .answer-box")
+  console.log("children", flipSquares.children)
+
 })
 
 document.querySelector("#pig-latin button").addEventListener('click',function(){
